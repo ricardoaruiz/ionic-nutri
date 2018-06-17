f4837b8f219bd352b7c55faff752c39d5aa12ebd
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2018-06-16 22:44:39 -0300

    Implementando Infinite Scroll e Refresh

--- src/pages/dicas/dicas.ts
+++ src/pages/dicas/dicas.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-import { IonicPage, LoadingController } from 'ionic-angular';
+import { IonicPage, LoadingController, InfiniteScroll } from 'ionic-angular';
 
 import { NavegacaoService } from './../../services/navegacao.service';
 import { WordpressService } from '../../services/wordpress.service';
 import { Post } from '../../services/model/post.model';
+
+import { HomePage } from './../home/home';
 import { PostPage } from '../post/post';
 
 @IonicPage()
@@ -22,33 +24,69 @@ export class DicasPage {
 
   }
 
-  ionViewWillEnter(){
-   
+  ionViewWillEnter(){   
     if (this.posts.length === 0) {
-      let loading = this.loadingCtrl.create();
-      loading.present();
-
-      this.wordpress.obtemPostsRecentes()
-        .subscribe(
-          (data: Post[]) => {
-            data.forEach( (post: Post) => {
-              post.excerpt.rendered = post.excerpt.rendered.split('<a')[0] + '<p>';
-              this.posts.push(post);
-            });
-            console.log(this.posts);
-            loading.dismiss();
-          },
-          error => {
-            console.log('Erro ao obter as Postagens', error);
-            loading.dismiss();
-          }
-        );
-    }
-    
+      this.carregaPosts();
+    }    
   }
 
   leiaMais(event, post) {
     this.nav.push(PostPage, {item: post});
+  }
+
+  doRefresh(refresher) {
+    this.carregaPosts(refresher);
+  }
+
+  doInfinite(infinteScrool) {
+    this.carregaPosts(undefined, infinteScrool);
+  }  
+
+  private carregaPosts(refresher?, infinteScrool?) {
+
+    let loading = this.loadingCtrl.create({content : 'Carregando...'});
+    let page = this.posts.length === 0 ? 1 : (Math.ceil(this.posts.length/10)) + 1;
+    let isLoading = true;    
+
+    if (refresher) {
+      page = 1
+      this.posts = [];
+      this.morePagesAvailable = true;
+    }
+
+    if (!infinteScrool && !refresher) loading.present();
+
+    this.wordpress.obtemPostsRecentes(page)
+        .subscribe(
+          (data: Post[]) => {
+            data.forEach( (post: Post) => {
+              if (infinteScrool && !isLoading) {
+                infinteScrool.complete();
+              }
+              post.excerpt.rendered = post.excerpt.rendered.split('<a')[0] + '<p>';
+              this.posts.push(post);
+              isLoading = false;
+            });
+            if (!refresher ) {
+              loading.dismiss();
+            } else  {
+              refresher.complete();
+            }
+          },
+          error => {
+            console.log('Erro ao obter as Postagens', error);
+            if (!refresher ) {
+              loading.dismiss();
+            } else  {
+              refresher.complete();
+            }
+
+            if (infinteScrool) {
+              infinteScrool.complete();
+              this.morePagesAvailable = false;
+            }
+          }
+        );
   }
 
 }
--- src/services/navegacao.service.ts
+++ src/services/navegacao.service.ts
@@ -45,4 +45,8 @@ export class NavegacaoService {
     }
   }
 
+  public getActiveComponent() {
+    this.nav.getActive().component;
+  }
+
 }
